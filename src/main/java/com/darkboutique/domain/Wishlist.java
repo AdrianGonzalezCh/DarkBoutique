0e10ffc061bf64137ee578f8821916a73557bcb5
Adria <Adria@Pc-Principal>
2025-03-16 13:44:42 -0600

    Version 01 de DarkBoutique, con la funcion CRUD en productos, y simulacion de carrito , darkstyle.css para definir el estilo "emo", del proyecto, Package creados en com, controller, dao, domain, service y serviceimpl

---
+++ src/main/java/com/darkboutique/domain/Wishlist.java
@@ -0,0 +1,57 @@
+package com.darkboutique.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "wishlist")
+public class Wishlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
+
+    // Constructor sin parámetros
+    public Wishlist() {
+    }
+
+    // Constructor con todos los parámetros
+    public Wishlist(Long id, Usuario usuario, Producto producto) {
+        this.id = id;
+        this.usuario = usuario;
+        this.producto = producto;
+    }
+
+    // GETTERS y SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
